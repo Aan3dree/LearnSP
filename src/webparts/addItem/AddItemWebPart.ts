2fd54ea70f86874beb731f79a8dc5a4264cd35ec
André Luiz <an3dree@gmail.com>
2021-10-08 08:42:17 -0300

    first commit

---
+++ src/webparts/addItem/AddItemWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'AddItemWebPartStrings';
+import AddItem from './components/AddItem';
+import { IAddItemProps } from './components/IAddItemProps';
+
+export interface IAddItemWebPartProps {
+  description: string;
+}
+
+export default class AddItemWebPart extends BaseClientSideWebPart<IAddItemWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IAddItemProps> = React.createElement(
+      AddItem,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
