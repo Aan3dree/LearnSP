cc5b9f4c8ed6ba6771f0a0d294b347f4cc4a547e
André Luiz <an3dree@gmail.com>
2021-10-08 21:05:34 -0300

    material ui list

--- src/webparts/addItem/AddItemWebPart.ts
+++ src/webparts/addItem/AddItemWebPart.ts
@@ -5,23 +5,47 @@ import {
   IPropertyPaneConfiguration,
   PropertyPaneTextField
 } from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { BaseClientSideWebPart, WebPartContext } from '@microsoft/sp-webpart-base';
 
 import * as strings from 'AddItemWebPartStrings';
 import AddItem from './components/AddItem';
 import { IAddItemProps } from './components/IAddItemProps';
+import { PropertyFieldListPicker, PropertyFieldListPickerOrderBy } from '@pnp/spfx-property-controls/lib/PropertyFieldListPicker';
+import { sp } from "@pnp/sp";
+import "@pnp/sp/webs";
+import "@pnp/sp/lists";
+import {setup as pnpSetup} from '@pnp/common'
+
+
 
 export interface IAddItemWebPartProps {
   description: string;
+  context: WebPartContext;
+  listName: string;
+  lists: string;
 }
 
+
 export default class AddItemWebPart extends BaseClientSideWebPart<IAddItemWebPartProps> {
+
+
+  protected onInit(): Promise<void>{
+    return super.onInit().then(_ =>{
+
+      pnpSetup({
+        spfxContext: this.context
+      })
+    })
+  }
 
   public render(): void {
     const element: React.ReactElement<IAddItemProps> = React.createElement(
       AddItem,
       {
-        description: this.properties.description
+        description: this.properties.description,
+        context: this.properties.context,
+        listName: this.properties.listName,
+        lists: this.properties.lists
       }
     );
 
@@ -32,9 +56,7 @@ export default class AddItemWebPart extends BaseClientSideWebPart<IAddItemWebPar
     ReactDom.unmountComponentAtNode(this.domElement);
   }
 
-  protected get dataVersion(): Version {
-    return Version.parse('1.0');
-  }
+
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
@@ -49,6 +71,22 @@ export default class AddItemWebPart extends BaseClientSideWebPart<IAddItemWebPar
               groupFields: [
                 PropertyPaneTextField('description', {
                   label: strings.DescriptionFieldLabel
+                }),
+                PropertyPaneTextField('listName', {
+                  label: "Lista"
+                }),
+                PropertyFieldListPicker('lists', {
+                  label: 'Select a list',
+                  selectedList: this.properties.lists,
+                  includeHidden: false,
+                  orderBy: PropertyFieldListPickerOrderBy.Title,
+                  disabled: false,
+                  onPropertyChange: this.onPropertyPaneFieldChanged.bind(this),
+                  properties: this.properties,
+                  context: this.context,
+                  onGetErrorMessage: null,
+                  deferredValidationTime: 0,
+                  key: 'listPickerFieldId'
                 })
               ]
             }
--- src/webparts/addItem/components/AddItem.tsx
+++ src/webparts/addItem/components/AddItem.tsx
@@ -2,22 +2,114 @@ import * as React from 'react';
 import styles from './AddItem.module.scss';
 import { IAddItemProps } from './IAddItemProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { sp } from "@pnp/sp";
+import "@pnp/sp/webs";
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { IconButton } from '@material-ui/core';
 
-export default class AddItem extends React.Component<IAddItemProps, {}> {
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+  return { name, calories, fat, carbs, protein };
+}
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export interface IColaboradores{
+  listItems: any;
+}
+
+export default class AddItem extends React.Component<IAddItemProps, IColaboradores,{}> {
+
+
+
+  constructor(props){
+    super(props);
+    this.state = {
+      listItems: []
+    }
+  }
+  async componentDidMount(){
+    const items: any[] = await sp.web.lists.getByTitle("Colaboradores").items.get();
+    console.log(items);
+
+    this.setState({
+      listItems: items
+    })
+
+  }
+
+  renderTable(value){
+    return (
+      <th>
+        <td>{value.Title}</td>
+        <td>{value.Cargo}</td>
+      </th>
+    )
+  }
+
+  renderMatTable(value){
+    return (
+      <th>
+        <td>{value.Title}</td>
+        <td>{value.Cargo}</td>
+      </th>
+    )
+  }
+
   public render(): React.ReactElement<IAddItemProps> {
     return (
       <div className={ styles.addItem }>
         <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
+        <TableContainer component={Paper}>
+            <Table className={styles.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Nome</TableCell>
+                  <TableCell align="center">Matricula</TableCell>
+                  <TableCell align="center">Departamento</TableCell>
+                  <TableCell align="center">Cargo</TableCell>
+                  <TableCell align="center">#</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.listItems.map((row) => (
+                  <TableRow key={row.Title}>
+                    <TableCell component="th" scope="row">
+                      {row.Title}
+                    </TableCell>
+                    <TableCell align="center">{row.Matricula}</TableCell>
+                    <TableCell align="center">{row.Departamento}</TableCell>
+                    <TableCell align="center">{row.Cargo}</TableCell>
+                    <TableCell align="center">
+                      <IconButton aria-label="delete">
+                        <DeleteIcon />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </div>
       </div>
     );
